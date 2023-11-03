f70319b8c87458f888e309a256dfe78a12a7d950
Subhajit Mandal <108950928+Subhajit-mn@users.noreply.github.com>
2023-11-03 10:53:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
-function calculateMinCost() {
+function calculateMinCost(arr, n) {
   //your code here
-  
-  
-  
+	let arr1 = [];
+  for(let i=0; i<arr.length; i++){
+	  arr1.push(arr[i]);
+  }
+  arr1.sort(function (a, b) {
+  	return a-b;
+  });
+	let res = 0;
+  while(arr1.length > 1){
+	  let first = arr1.shift();
+	  let second = arr1.shift();
+	  res += first + second;
+	  arr1.push(first + second);
+	  arr1.sort(function(a, b){
+		  return a-b;
+	  });
+  }
+	return res;
 }  
